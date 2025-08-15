78ce69d88db7f31ad128481644c046339bdabe50
sakib-anik <sakib.within@gmail.com>
2025-08-16 00:45:18 +0600

    Sub Navigation Done

--- app/parallel-dashboard/@notifications/page.js
+++ app/parallel-dashboard/@notifications/page.js
@@ -1,6 +1,14 @@
 import wait from "@/lib/wait";
+import Link from "next/link";
 
 export default async function page() {
   await wait(8000);
-  return <h3>Notification Page</h3>;
+  return (
+    <>
+      <div className="" style={{ border: "2px solid red" }}>
+        <h3>Notification Page</h3>
+        <Link href="/parallel-dashboard/seen">Seen</Link>
+      </div>
+    </>
+  );
 }
---
+++ app/parallel-dashboard/@notifications/seen/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default async function page() {
+  return (
+    <>
+      <div className="" style={{ border: "2px solid green" }}>
+        <h3>Seen Page</h3>
+        <Link href="/parallel-dashboard">All</Link>
+      </div>
+    </>
+  );
+}
